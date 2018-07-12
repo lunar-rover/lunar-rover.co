e30fdf5ae4bb187595be15ad311a1ae5fe10f31d
John Wong <johnwong668@gmail.com>
2018-07-12 22:08:29 +0800

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require("nodemailer");
+const config = require("../bin/config");
+
+var smtpTransport = nodemailer.createTransport(config.email.nodemailer);
+
+function sendEmailCallback(error, response) {
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
+    }
+
+    // if you don't want to use this transport object anymore, uncomment following line
+    //smtpTransport.close(); // shut down the connection pool, no more messages
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/process-contact', function(req, res, next) {
+    if (req.param("name") && req.param("email") && req.param("message")) {
+        smtpTransport.sendMail({
+            from: req.param("email"),
+            to: config.email.mailTo,
+            subject: 'Contact Us by ' + req.param("name"),
+            text: req.param("message")
+        }, sendEmailCallback);
+
+        // TODO: send response email to requester
+
+        res.json({done: true});
+    }
+    res.json({error: 'Failed to send your message. Please try again later.'});
+});
+
+module.exports = router;
