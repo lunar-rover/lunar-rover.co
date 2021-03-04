99828845a5d41b284cb116a4d04a1a93051ff664
John Wong <johnwong668@gmail.com>
2021-03-04 21:46:30 +0800

    changed deployment ref

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user: 'ubuntu',
       host: 'ec2-54-159-190-1.compute-1.amazonaws.com',
       key: '~/.ssh/lunar-rover-co.pem',
-      ref: 'origin/master',
+      ref: 'origin/v20210127',
       repo: 'git@lunar-rover.co.github.com:lunar-rover/lunar-rover.co.git',
       path: '/home/ubuntu/lunar-rover.co',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
